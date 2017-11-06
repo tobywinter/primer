acce3a4b023679987ea4b31102fb58aa6dfdf706
Toby Winter <toby.hw@live.co.uk>
2017-11-06 02:35:49 +0000

    Adds returns false if input not a number

--- spec/inputCheckerSpec.js
+++ spec/inputCheckerSpec.js
@@ -15,6 +15,10 @@ describe('InputChecker', function() {
       expect(checker.checkInput(-5)).toEqual(false);
     });
 
+    it('returns false if input is not a number', function() {
+      expect(checker.checkInput('Five')).toEqual(false);
+    });
+
     it('returns true if input is valid', function() {
       expect(checker.checkInput(5)).toEqual(true);
     });
