e08d48bfdac4220afaf244f66f6bbc38c1b7e504
Toby Winter <toby.hw@live.co.uk>
2017-11-06 03:40:15 +0000

    Adds run method to index.js - accepts user input & prints table

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var rl = require('readline');
 const PrimeGenerator = require('./src/primeGenerator').PrimeGenerator;
-
+const MultiplicationTable = require('./src/multiplicationTable').MultiplicationTable;
+const InputChecker = require('./src/inputChecker').InputChecker;
 
 var read = rl.createInterface({
   input: process.stdin,
@@ -8,6 +9,20 @@ var read = rl.createInterface({
 });
 
 var run = function() {
-
-}
+  read.question("How many primes do you require?", function (input) {
+   if (new InputChecker().checkInput(input)) {
+     read.close();
+     var n = parseInt(input);
+     console.log("First " + n + " prime numbers:");
+     var primeGenerator = new PrimeGenerator();
+     var multiplicationTable = new MultiplicationTable();
+     var primes = primeGenerator.generate(n);
+     var table = multiplicationTable.tabulate(primes);
+     console.log(table);
+   } else {
+     console.log("Invalid input: Please input a whole integer greater than 1");
+     run();
+   }
+ });
+};
 run();
